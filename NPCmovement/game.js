8cd5a3a2ebd56ecc62dea755a35615c2d5c9dba1
Raszero <travisjcelli95@gmail.com>
2024-07-11 12:40:43 -0500

    fixed sizing issue. canvas was being stretched to 1000x500 but canvas.width & height had not actaully been set in game.js file

--- NPCmovement/game.js
+++ NPCmovement/game.js
@@ -7,12 +7,15 @@ const canvasHeight = 1000;
 const numberOfEnemies = 100;
 const enemyArray = [];
 
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
 class Enemy {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.height = 100;
-    this.width = 100;
+    this.height = 50;
+    this.width = 50;
     this.speed = Math.random() * 4 - 2;
   }
   update() {
