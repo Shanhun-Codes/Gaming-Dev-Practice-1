c5332e2195d1d957a39e872f41f147419c7f8578
Raszero <travisjcelli95@gmail.com>
2024-07-19 11:05:30 -0500

    completed animations on trigger tutorial

--- 05NPCmovement3/game.js
+++ 05NPCmovement3/game.js
@@ -4,30 +4,30 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const canvasWidth = (canvas.width = 500);
 const canvasHeight = (canvas.height = 1000);
-const numberOfEnemies = 50;
+const numberOfEnemies = 10;
 const enemyArray = [];
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "enemy2.png";
+    this.image.src = "enemy3.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.height = this.spriteHeight / 4;
     this.width = this.spriteWidth / 4;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
-    this.flapSpeed = Math.floor(Math.random() * 3) + 1;
-    this.angle = 0;
-    this.angleSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 10
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 500;
+    this.angleSpeed = Math.random() * 0.5 + 0.5
+    // this.curve = Math.random() * 200 + 50
   }
   update() {
-    this.x -= this.speed;
-    this.y += this.curve * Math.sin(this.angle);
+    this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/200) + (canvasWidth/2 - this.width/2)
+    this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/300) + (canvasHeight/2 - this.height/2)
     this.angle += this.angleSpeed
     if (this.x + this.width < 0) this.x = canvas.width;
     if (gameFrame % this.flapSpeed === 0) {
